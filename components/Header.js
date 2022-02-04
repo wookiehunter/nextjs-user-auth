59ad1225a9be73a7c3324459c3b9d592fcfdf416
Steven Godson <steven@xenos-design.co.uk>
2022-02-04 11:59:12 +0000

    initial next-auth changes

--- components/Header.js
+++ components/Header.js
@@ -1,29 +1,76 @@
-import { FormGroup, FormControlLabel, Switch } from '@mui/material';
-import { Icon } from '@mui/material';
-import Brightness6OutlinedIcon from '@mui/icons-material/Brightness6Outlined';
 import styles from '../styles/Header.module.css';
 import Link from 'next/link';
+import { SignInOptions, signOut, useSession } from 'next-auth/react';
 
 export default function Header() {
+	const { data: session, status } = useSession();
+	const loading = status === 'loading';
+
 	return (
-		<div id='header' className={styles.header}>
-			{/* <FormGroup>
-				<FormControlLabel
-					control={<Switch color='success' defaultChecked />}
-					label='Light/Dark'
-				/>
-			</FormGroup> */}
-			<ul className={styles.nav}>
-				<li>
-					<Link href='/'>Home</Link>
-				</li>
-				<li>
-					<Link href='api/auth/signin'>Login</Link>
-				</li>
-                <li>
-					<Link href='/signup'>Register</Link>
-				</li>
-			</ul>
-		</div>
+		<header id='header' className={styles.header}>
+			<noscript>
+				<style>{`.nojs-show {opacity: 1; top: 0; }`}</style>
+			</noscript>
+			<div className={styles.signedInStatus}>
+				<p
+					className={`nojs-show ${
+						!session && loading ? styles.loading : styles.loaded
+					}`}
+				>
+					{!session && (
+						<>
+							<span className={styles.notSignedInText}>
+								You are not signed in
+							</span>
+							<a
+								href={`/api/auth/signin`}
+								className={styles.buttonPrimary}
+								onClick={(e) => {
+									e.preventDefault();
+									signIn();
+								}}
+							>
+								Sign in
+							</a>
+						</>
+					)}
+					{session && (
+						<>
+							{session.user.image && (
+								<span
+									style={{ backgroundImage: `url('${session.user.image}')` }}
+									className={styles.avatar}
+								/>
+							)}
+							<span className={styles.signedInText}>
+								<small>Signed in as</small>
+								<br />
+								<strong>{session.user.email || session.user.name}</strong>
+							</span>
+							<a
+								href={`/api/auth/signout`}
+								className={styles.button}
+								onClick={(e) => {
+									e.preventDefault();
+									signOut();
+								}}
+							>
+								Sign out
+							</a>
+						</>
+					)}
+				</p>
+			</div>
+			<nav>
+				<ul className={styles.nav}>
+					<li>
+						<Link href='/'>Home</Link>
+					</li>
+					<li>
+						<Link href='/protected'>Protected</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
 	);
 }
