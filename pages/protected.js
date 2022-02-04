e5bed972d38ec1271f5692485eb885a6bcb043df
Steven Godson <steven@xenos-design.co.uk>
2022-02-04 12:07:12 +0000

    second next-auth change

--- pages/protected.js
+++ pages/protected.js
@@ -1,16 +1,34 @@
-import { getSession } from 'next-auth/react';
+import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import Layout from '../components/layout'
+import AccessDenied from '../components/AccessDenied'
 
-export default async (req, res) => {
-	const session = await getSession({ req });
+export default function Page () {
+  const { data: session, status } = useSession()
+	const loading = status === 'loading'
+  const [ content , setContent ] = useState()
 
-	if (session) {
-		res.send({
-			content:
-				'This is protected content. You can access this content because you are signed in.',
-		});
-	} else {
-		res.send({
-			error: 'You must be sign in to view the protected content on this page.',
-		});
-	}
-};
+  // Fetch content from protected route
+  useEffect(()=>{
+    const fetchData = async () => {
+      const res = await fetch('/api/examples/protected')
+      const json = await res.json()
+      if (json.content) { setContent(json.content) }
+    }
+    fetchData()
+  },[session])
+
+  // When rendering client side don't display anything until loading is complete
+  if (typeof window !== 'undefined' && loading) return null
+
+  // If no session exists, display access denied message
+  if (!session) { return  <Layout><AccessDenied/></Layout> }
+
+  // If session exists, display content
+  return (
+    <Layout>
+      <h1>Protected Page</h1>
+      <p><strong>{content || "\u00a0"}</strong></p>
+    </Layout>
+  )
+}
